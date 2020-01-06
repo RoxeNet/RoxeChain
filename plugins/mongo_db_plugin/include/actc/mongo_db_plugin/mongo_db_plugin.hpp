a1fcddedbcabd7a60b4676606a3f9a0ba0fc4762
Sharker <wshadows@protonmail.com>
2020-01-06 23:57:44 +0800

    update mongodb

--- plugins/mongo_db_plugin/include/actc/mongo_db_plugin/mongo_db_plugin.hpp
+++ plugins/mongo_db_plugin/include/actc/mongo_db_plugin/mongo_db_plugin.hpp
@@ -1,14 +1,14 @@
 /**
  *  @file
- *  @copyright defined in dcc/LICENSE.txt
+ *  @copyright defined in actc/LICENSE.txt
  */
 #pragma once
 
-#include <dccio/chain_plugin/chain_plugin.hpp>
+#include <actc/chain_plugin/chain_plugin.hpp>
 #include <appbase/application.hpp>
 #include <memory>
 
-namespace dccio {
+namespace actc {
 
 using mongo_db_plugin_impl_ptr = std::shared_ptr<class mongo_db_plugin_impl>;
 
@@ -23,7 +23,7 @@ using mongo_db_plugin_impl_ptr = std::shared_ptr<class mongo_db_plugin_impl>;
  * pub_keys
  * account_controls
  *
- *   See data dictionary (DB Schema Definition - dcc API) for description of MongoDB schema.
+ *   See data dictionary (DB Schema Definition - actc API) for description of MongoDB schema.
  *
  *   If cmake -DBUILD_MONGO_DB_PLUGIN=true  not specified then this plugin not compiled/included.
  */
