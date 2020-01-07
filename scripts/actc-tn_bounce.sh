fac7713f6dac4a4eb2e72783070a1b23999c121e
TerryYao <toyaowu@163.com>
2020-01-07 15:46:46 +0800

    update scripts

--- scripts/actc-tn_bounce.sh
+++ scripts/actc-tn_bounce.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 #
-# dccio-tn_bounce is used to restart a node that is acting badly or is down.
-# usage: dccio-tn_bounce.sh [arglist]
+# actc-tn_bounce is used to restart a node that is acting badly or is down.
+# usage: actc-tn_bounce.sh [arglist]
 # arglist will be passed to the node's command line. First with no modifiers
 # then with --hard-replay-blockchain and then a third time with --delete-all-blocks
 #
 # the data directory and log file are set by this script. Do not pass them on
 # the command line.
 #
-# in most cases, simply running ./dccio-tn_bounce.sh is sufficient.
+# in most cases, simply running ./actc-tn_bounce.sh is sufficient.
 #
 
-pushd $dccIO_HOME
+pushd $actc_HOME
 
-if [ ! -f programs/noddcc/noddcc ]; then
-    echo unable to locate binary for noddcc
+if [ ! -f programs/nodactc/nodactc ]; then
+    echo unable to locate binary for nodactc
     exit 1
 fi
 
-config_base=etc/dccio/node_
-if [ -z "$dccIO_NODE" ]; then
+config_base=etc/actc/node_
+if [ -z "$actc_NODE" ]; then
     DD=`ls -d ${config_base}[012]?`
     ddcount=`echo $DD | wc -w`
     if [ $ddcount -ne 1 ]; then
         echo $HOSTNAME has $ddcount config directories, bounce not possible. Set environment variable
-        echo dccIO_NODE to the 2-digit node id number to specify which node to bounce. For example:
-        echo dccIO_NODE=06 $0 \<options\>
+        echo actc_NODE to the 2-digit node id number to specify which node to bounce. For example:
+        echo actc_NODE=06 $0 \<options\>
         cd -
         exit 1
     fi
     OFS=$((${#DD}-2))
-    export dccIO_NODE=${DD:$OFS}
+    export actc_NODE=${DD:$OFS}
 else
-    DD=${config_base}$dccIO_NODE
+    DD=${config_base}$actc_NODE
     if [ ! \( -d $DD \) ]; then
         echo no directory named $PWD/$DD
         cd -
@@ -40,5 +40,5 @@ else
     fi
 fi
 
-bash $dccIO_HOME/scripts/dccio-tn_down.sh
-bash $dccIO_HOME/scripts/dccio-tn_up.sh "$*"
+bash $actc_HOME/scripts/actc-tn_down.sh
+bash $actc_HOME/scripts/actc-tn_up.sh "$*"
