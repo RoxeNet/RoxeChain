87975d50241405a69eceedd5e93cbd977eff6324
dwjpeng <dwjpeng@126.com>
2020-05-11 09:54:04 +0800

    "remove invalid code"

--- roxe.cdt_old/libraries/libc/musl/include/bits/stddef.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#ifdef ROXE_NATIVE
-   typedef unsigned long size_t;
-   typedef long ptrdiff_t;
-#else
-   typedef unsigned int size_t;
-   typedef int ptrdiff_t; //XXX intptr_t
-#endif
