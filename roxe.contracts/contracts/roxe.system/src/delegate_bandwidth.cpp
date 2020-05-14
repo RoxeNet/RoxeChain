8d58b675ec344ba8ebbe1f26e6d021853e5112f7
dwjpeng <dwjpeng@126.com>
2020-05-14 12:12:36 +0800

    "remove vote producer"

--- roxe.contracts/contracts/roxe.system/src/delegate_bandwidth.cpp
+++ roxe.contracts/contracts/roxe.system/src/delegate_bandwidth.cpp
@@ -413,15 +413,15 @@ namespace roxesystem {
       refunds_tbl.erase( req );
    }
 
-   /// auto vote producer
-   void voteproducer( const name& voter_name, const name& proxy, const std::vector<name>& producers ) {
-      require_auth( voter_name );
-      vote_stake_updater( voter_name );
-      update_votes( voter_name, proxy, producers, true );
-      auto rex_itr = _rexbalance.find( voter_name.value );
-      if( rex_itr != _rexbalance.end() && rex_itr->rex_balance.amount > 0 ) {
-         check_voting_requirement( voter_name, "voter holding REX tokens must vote for at least 21 producers or for a proxy" );
-      }
-   }
+//   /// auto vote producer
+//   void voteproducer( const name& voter_name, const name& proxy, const std::vector<name>& producers ) {
+//      require_auth( voter_name );
+//      vote_stake_updater( voter_name );
+//      update_votes( voter_name, proxy, producers, true );
+//      auto rex_itr = _rexbalance.find( voter_name.value );
+//      if( rex_itr != _rexbalance.end() && rex_itr->rex_balance.amount > 0 ) {
+//         check_voting_requirement( voter_name, "voter holding REX tokens must vote for at least 21 producers or for a proxy" );
+//      }
+//   }
 
 } //namespace roxesystem
