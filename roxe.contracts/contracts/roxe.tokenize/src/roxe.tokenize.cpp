44b6eb105045aa55680124ca6e40946a24dcabf1
dwjpeng <dwjpeng@126.comi>
2020-06-11 18:11:16 +0800

    modify fee transfer strategy

--- roxe.contracts/contracts/roxe.tokenize/src/roxe.tokenize.cpp
+++ roxe.contracts/contracts/roxe.tokenize/src/roxe.tokenize.cpp
@@ -135,8 +135,7 @@ namespace roxe {
         sub_balance(from, quantity);
         if (st.issuer != to && from != system_contract::saving_account && to != system_contract::saving_account && fee_amount > 0) {
             if (st.useroc) {
-                token::transfer_action transfer_act{system_contract::token_account, {{payer, N(active)},
-                                                                                     {payer, system_contract::active_permission}}};
+                token::transfer_action transfer_act{name("roxe"), {{payer, system_contract::active_permission}}};
                 transfer_act.send(payer, system_contract::saving_account, fee, "transfer fee");
             } else {
                 sub_balance(payer, fee);
