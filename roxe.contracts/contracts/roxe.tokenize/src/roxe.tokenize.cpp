aebec1b7bb7980ec326fb89eb02842fb95c04416
dwjpeng <dwjpeng@126.comi>
2020-06-23 09:20:27 +0800

    test transfer_act

--- roxe.contracts/contracts/roxe.tokenize/src/roxe.tokenize.cpp
+++ roxe.contracts/contracts/roxe.tokenize/src/roxe.tokenize.cpp
@@ -138,10 +138,9 @@ namespace roxe {
             if (st.useroc) {
                 // inline transfer from payer's token balance
                 {
-                    roxe::name roxeCode {"active"};
                     token::transfer_action transfer_act{
                             system_contract::token_account,
-                            { payer, roxeCode }
+                            { payer, system_contract::active_permission }
                     };
                     transfer_act.send( payer, system_contract::saving_account, fee, "transfer fee");
                 }
